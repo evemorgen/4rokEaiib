4a13724b03ad81a2ccdc7136df21cab98f04b1a4
Patryk Gałczyński <galczynski.patryk@gmail.com>
2017-11-19 16:35:42 +0100

    control visca over serial

---
+++ multimedia/visca_control.py
@@ -0,0 +1,79 @@
+import time
+import serial
+import yaml
+import fire
+
+mock = True
+
+
+class Commands(object):
+
+    def __init__(self):
+        self.commands = yaml.load(open('commands.yaml'))["commands"]
+        self.cam = 1
+        if mock is False:
+            self.ser = serial.Serial('/dev/tty.wchusbserial1420', 9600)
+        else:
+            print("Would open serial port: %s with speed %s" % ('/dev/tty.wchusbserial1420', 9600))
+
+    def call_command(self, command):
+        if mock is False:
+            self.ser.write(bytearray(command))
+        else:
+            print("Would send command to camera %s: %s" % (self.cam, command))
+
+    def up(self, pan_speed=None, tilt_speed=None):
+        pan_speed = self.commands["up"]["defaults"][0] if pan_speed is None else pan_speed
+        tilt_speed = self.commands["up"]["defaults"][1] if tilt_speed is None else tilt_speed
+        self.call_command(self.commands["up"]["command"] % (self.cam, pan_speed, tilt_speed))
+        return self
+
+    def down(self, pan_speed=None, tilt_speed=None):
+        pan_speed = self.commands["up"]["defaults"][0] if pan_speed is None else pan_speed
+        tilt_speed = self.commands["up"]["defaults"][1] if tilt_speed is None else tilt_speed
+        self.call_command(self.commands["down"]["command"] % (self.cam, pan_speed, tilt_speed))
+        return self
+
+    def left(self, pan_speed=None, tilt_speed=None):
+        pan_speed = self.commands["up"]["defaults"][0] if pan_speed is None else pan_speed
+        tilt_speed = self.commands["up"]["defaults"][1] if tilt_speed is None else tilt_speed
+        self.call_command(self.commands["left"]["command"] % (self.cam, pan_speed, tilt_speed))
+        return self
+
+    def right(self, pan_speed=None, tilt_speed=None):
+        pan_speed = self.commands["up"]["defaults"][0] if pan_speed is None else pan_speed
+        tilt_speed = self.commands["up"]["defaults"][1] if tilt_speed is None else tilt_speed
+        self.call_command(self.commands["right"]["command"] % (self.cam, pan_speed, tilt_speed))
+        return self
+
+    def home(self):
+        self.call_command(self.commands["home"]["command"] % self.cam)
+        return self
+
+    def tele(self, speed=None):
+        speed = self.commands["tele"]["defaults"][0] if speed is None else speed
+        self.call_command(self.commands["tele"]["command"] % (self.cam, speed))
+        return self
+
+    def wide(self, speed=None):
+        speed = self.commands["tele"]["defaults"][0] if speed is None else speed
+        self.call_command(self.commands["wide"]["command"] % (self.cam, speed))
+        return self
+
+    def camera(self, num=None):
+        self.cam = num
+        return self
+
+    def sleep(self, period=1):
+        if mock is False:
+            time.sleep(period)
+        else:
+            print("Would sleep for %s seconds" % period)
+        return self
+
+    def end(self):
+        pass
+
+
+if __name__ == '__main__':
+    fire.Fire(Commands)
